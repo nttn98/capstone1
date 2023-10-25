c3ffeca5262338b9e99ab446edca9263e52615e7
nttn98 <nguyentrantrinhan1998@gmail.com>
2023-10-25 10:04:01 +0700

    25/10

---
+++ src/main/java/com/capstone1/model/Admin.java
@@ -0,0 +1,38 @@
+package com.capstone1.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adminId;
+    private String adminUser;
+    private String adminPassword;
+
+    public Admin() {
+    }
+
+    public String getAdminUser() {
+        return adminUser;
+    }
+
+    public void setAdminUser(String adminUser) {
+        this.adminUser = adminUser;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    public Long getAdminId() {
+        return adminId;
+    }
+
+}
