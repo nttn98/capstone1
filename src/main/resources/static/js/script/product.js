3a0a34e854b2674dbf7e56025d0f3d0786a41521
nttn98 <nguyentrantrinhan1998@gmail.com>
2023-12-21 13:41:23 +0700

    ok

---
+++ src/main/resources/static/js/script/product.js
@@ -0,0 +1,46 @@
+function showChangePass ()
+{
+    $( '.changePass' ).addClass( 'w-25' );
+    $( '.profile' ).removeClass( "w-25" );
+}
+function closeChangePass ()
+{
+    $( '.changePass' ).removeClass( "w-25" );
+    $( '.closeMask' ).fadeOut();
+
+}
+function showProfile ()
+{
+    $( '.profile' ).addClass( "w-25" );
+    $( '.changePass' ).removeClass( "w-25" );
+    $( '.closeMask' ).fadeIn();
+}
+function closeProfile ()
+{
+    $( '.profile' ).removeClass( "w-25" );
+    $( '.closeMask' ).fadeOut();
+}
+$( '.closeMask' ).click( ( e ) =>
+{
+    if ( e.target.classList.contains( 'closeMask' ) )
+        $( 'button.close.close-mask' ).click();
+} );
+
+$( document ).ready( () =>
+{
+    $( '.formatPrice' ).each( ( i, e ) =>
+    {
+        let val = +( $( e ).text() );
+        $( e ).text( formatMoney( val ) );
+    } );
+
+    function formatMoney ( number )
+    {
+        return number.toLocaleString( 'vi-VN', {
+            style: 'currency', currency: 'VND'
+        } );
+    }
+} );
+
+alert_action(/*[[${ alert }]]*/ )
+
